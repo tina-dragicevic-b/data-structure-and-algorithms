766d3a1a2d63de5cf82dcc83c26c579b3b8919c6
tina-dragicevic <tina.dragicevic3108@gmail.com>
2023-02-23 13:42:50 +0100

    Queue added

--- data-structure-and-algorithms/src/Main.java
+++ data-structure-and-algorithms/src/Main.java
@@ -1,26 +1,52 @@
+import dataStructures1.Queue;
 import dataStructures1.Stack;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world!");
-        System.out.println(args.length);
+//        System.out.println("Hello world!");
+//        System.out.println(args.length);
+        final int SIZE = 5;
+
 
     // Stack
-        var stack = new Stack(4);
-        stack.push(5);
-        stack.print();
-        stack.push(4);
-        stack.print();
-        stack.push(8);
-        stack.print();
-        stack.push(11);
-        stack.print();
-        stack.push(4);
-        stack.print();
-        stack.pop();
-        stack.print();
-        stack.pop();
-        stack.print();
-        Thread.sleep(3500);
+
+//        var stack = new Stack(4);
+//        stack.push(5);
+//        stack.print();
+//        stack.push(4);
+//        stack.print();
+//        stack.push(8);
+//        stack.print();
+//        stack.push(11);
+//        stack.print();
+//        stack.push(4);
+//        stack.print();
+//        stack.pop();
+//        stack.print();
+//        stack.pop();
+//        stack.print();
+//        Thread.sleep(3500);
+
+
+    // Queue
+        var que = new Queue(SIZE);
+        que.dequeue();
+        que.enqueue(5);
+        que.enqueue(2);
+        que.enqueue(45);
+        que.enqueue(5);
+        que.enqueue(61);
+        que.print();
+        que.enqueue(28);
+        que.dequeue();
+        que.dequeue();
+        que.print();
+        que.enqueue(28);
+        que.print();
+        que.enqueue(57);
+        que.print();
+        que.enqueue(28);
+        que.print();
+
     }
 }
---
+++ data-structure-and-algorithms/src/dataStructures1/Queue.java
@@ -0,0 +1,74 @@
+package dataStructures1;
+
+import java.util.ArrayList;
+
+public class Queue {
+//    Queue always keeps track of the first element/front (does not have to be at index 0) and the last one/rear.
+//    FIFO rule
+    ArrayList<Integer> queue;
+    int SIZE, front, rear;
+
+    public Queue(int size){
+        this.SIZE = size;
+        this.queue = new ArrayList<>(this.SIZE);
+        this.front = this.rear = -1;
+    }
+    private boolean isFull(){
+//        return this.queue.size() == this.SIZE;
+        return this.rear == this.SIZE - 1;
+    }
+    private boolean isEmpty(){
+        return this.queue.isEmpty() || this.front == -1;
+    }
+    public void enqueue(int element){
+        if(!isFull()){
+            this.queue.add(element);
+            if(this.front >= 0){
+                this.rear++;
+            } else {
+                this.front = 0;
+                this.rear = 0;
+            }
+        } else {
+            if(this.rear == this.SIZE - 1 && this.front > 0){
+                //(this.rear - this.front < this.SIZE - 1)
+                // move all elements one position left and place new element at the end
+                var tempQueue = new ArrayList<Integer>();
+                for(int i = 0; i < this.rear; i++){
+                    if(i < this.front - 1){
+                        tempQueue.add(0);
+                    } else tempQueue.add(i, this.queue.get(i + 1));
+                }
+                System.out.println("tempQueue " + tempQueue);
+                this.queue = tempQueue;
+                this.front--;
+//                this.rear--;
+                this.queue.add(element);
+            } else System.out.println("Queue is full. ");
+        }
+    }
+
+    public void dequeue(){
+        // eject the first element inserted (at index 0)
+        if(!isEmpty()){
+            var ejected = this.queue.get(this.front);
+            if(this.front == this.rear){
+                this.front = this.rear = -1;
+                System.out.println("Deleted element is: " + ejected);
+                System.out.println("All queue elements are deleted.");
+            } else {
+                this.front++;
+            }
+        } else {
+            System.out.println("Queue is empty. ");
+        }
+    }
+    public void print(){
+        System.out.println("Position of front: " + this.front + ", and rear: " + this.rear);
+        for(int i = this.front; i <= this.rear; i++){
+            System.out.print(queue.get(i) + " ");
+        }
+//        System.out.println(this.queue);
+        System.out.println();
+    }
+}
